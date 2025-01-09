6bbf214bf3742490b30e41de9ee40c8d7c030d05
chiragh999 <chiraghirapra999@gmail.com>
2025-01-09 11:24:13 +0530

    changes

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
      path("category/<int:pk>/",CategoryGetViewSet.as_view()),
      path("item/",ItemViewSet.as_view()),
      path("item/<int:pk>/",ItemGetViewSet.as_view()),
-     path("event/",EventBookingViewSet.as_view()),
-     path("evnet/<int:pk>/",EventBookingGetViewSet.as_view()),
+     path("event-bookings/",EventBookingViewSet.as_view()),
+     path("event-bookings/<int:pk>/",EventBookingGetViewSet.as_view()),
      path("eventfilter/",EventFilterViewSet.as_view()),
      
 ]
