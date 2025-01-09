75b32b1cd64eb00e6a888b40d98245f0148187cd
chiragh999 <chiraghirapra999@gmail.com>
2025-01-09 12:04:29 +0530

    changes

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 urlpatterns = [
      path("login/",LoginViewSet.as_view()),
-     path("category/",CategoryViewSet.as_view()),
-     path("category/<int:pk>/",CategoryGetViewSet.as_view()),
-     path("item/",ItemViewSet.as_view()),
-     path("item/<int:pk>/",ItemGetViewSet.as_view()),
+     path("categorys/",CategoryViewSet.as_view()),
+     path("categorys/<int:pk>/",CategoryGetViewSet.as_view()),
+     path("items/",ItemViewSet.as_view()),
+     path("items/<int:pk>/",ItemGetViewSet.as_view()),
      path("event-bookings/",EventBookingViewSet.as_view()),
      path("event-bookings/<int:pk>/",EventBookingGetViewSet.as_view()),
      path("eventfilter/",EventFilterViewSet.as_view()),
