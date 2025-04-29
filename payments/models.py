f5658acd6e039876390a3237c38193174cf264ca
chiragh999 <chiraghirapra999@gmail.com>
2025-04-29 12:44:27 +0530

    changes

--- payments/models.py
+++ payments/models.py
@@ -40,6 +40,7 @@ class Payment(models.Model):
     note = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    rule = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Payment {self.bill_no}"
