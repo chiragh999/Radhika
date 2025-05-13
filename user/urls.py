e18a9fe1b9a1f4135db202c83d58066b7d660d7e
chiragh999 <chiraghirapra999@gmail.com>
2025-05-13 10:49:34 +0530

    changes

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("login/", LoginViewSet.as_view()),
     path("add-note/",NoteViewSet.as_view()),
     path("get-note/",NoteViewSet.as_view()),
-    path("update-note/<int:pk>",NoteViewSet.as_view())
+    path("update-note/<int:pk>/",NoteViewSet.as_view())
 ]
